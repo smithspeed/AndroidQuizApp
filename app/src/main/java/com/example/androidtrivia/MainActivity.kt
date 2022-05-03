3753fe91f85cf2e0c54e4d01b03bc4001a78e7cb
dhananjaykumar <dhananjayspeed@gmail.com>
2022-05-03 16:05:51 +0530

    upds

--- app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -2,28 +2,62 @@ package com.example.androidtrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.androidtrivia.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var navController : NavController
+    private lateinit var drawerLayout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        navController = this.findNavController(R.id.navControllerFragment)
+        drawerLayout = binding.drawerLayout
 
+        navController = this.findNavController(R.id.navControllerFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        binding.navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        //return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.about -> {
+
+                drawerLayout.closeDrawer(GravityCompat.START)
+
+                navController.navigate(R.id.action_homeFragment2_to_aboutFragment)
+
+            }
+            R.id.rules -> {
+
+                drawerLayout.closeDrawer(GravityCompat.START)
+                navController.navigate(R.id.action_homeFragment2_to_rulesFragment)
+            }
+
+        }
+
+        return NavigationUI.onNavDestinationSelected(item, navController)
+
     }
 }
